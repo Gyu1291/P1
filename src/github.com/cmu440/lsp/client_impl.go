ac2ed910c311a3aaf41f779b9ee522d01153dd03
cactusnoh <cactusnoh@gmail.com>
2022-12-03 19:18:20 +0900

    Tests run fine

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,88 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/cmu440/lspnet"
+)
 
 type client struct {
-	// TODO: implement this!
+	id     int // connection ID of client
+	params *Params
+	conn   *lspnet.UDPConn // connection to server
+
+	currEpoch int // current time
+
+	closeEpoch        int // epoch when Close() was called
+	timeoutCloseEpoch int // epoch when server was closed by timeout
+
+	server ConnectionInfo
+
+	queryChan chan Query
+
+	readChan  chan *Message
+	closeChan chan int
+
+	processingEnd chan bool
+	timerEnd      chan bool
+}
+
+func connectToServer(conn *lspnet.UDPConn, hostport string, initialSeqNum int, params *Params) (int, error) {
+	// Send Connect message to server
+	msg := NewConnect(initialSeqNum)
+	msg.Checksum = CalculateChecksum(msg.ConnID, msg.SeqNum, msg.Size, msg.Payload)
+	connectMsg, err := json.Marshal(msg)
+	if err != nil {
+		return -1, err
+	}
+	_, err = conn.Write(connectMsg)
+	if err != nil {
+		return -1, err
+	}
+
+	ackChan := make(chan *Message)
+	go receiveConnAck(conn, ackChan)
+
+	epochCount := 0
+	for {
+		timer := time.NewTimer(time.Millisecond * time.Duration(params.EpochMillis))
+		select {
+		case <-timer.C:
+			epochCount++
+			if epochCount == params.EpochLimit {
+				return -1, errors.New("connection timeout")
+			}
+			// Send Connect message to server again
+			_, err := conn.Write(connectMsg)
+			if err != nil {
+				return -1, err
+			}
+		case connAck := <-ackChan:
+			return connAck.ConnID, nil
+		}
+	}
+}
+
+func receiveConnAck(conn *lspnet.UDPConn, ackChan chan *Message) {
+	buffer := make([]byte, 1500)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			continue
+		}
+
+		msg := &Message{}
+		err = json.Unmarshal(buffer[:n], msg)
+		if err != nil {
+			continue
+		}
+
+		ackChan <- msg // received conn ack
+		return
+	}
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -22,23 +100,366 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, initialSeqNum int, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+	raddr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := lspnet.DialUDP("udp", nil, raddr)
+	if err != nil {
+		return nil, err
+	}
+
+	connId, err := connectToServer(conn, hostport, initialSeqNum, params)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	c := &client{
+		id:     connId,
+		params: params,
+		conn:   conn,
+
+		currEpoch: 0,
+
+		closeEpoch:        -1,
+		timeoutCloseEpoch: -1,
+
+		server: ConnectionInfo{
+			id:   0,
+			addr: *raddr,
+
+			lastRecvEpoch: 0,
+			lastSendEpoch: 0,
+
+			sendBase:   initialSeqNum + 1,
+			nextSeqnum: initialSeqNum + 1,
+			lastSeqnum: initialSeqNum,
+			sendBuf:    make(map[int]*SendMessage),
+
+			recvBase: 1,
+			readBase: 1,
+			recvBuf:  make(map[int]*Message),
+		},
+
+		queryChan: make(chan Query),
+
+		readChan:  make(chan *Message),
+		closeChan: make(chan int),
+
+		processingEnd: make(chan bool),
+		timerEnd:      make(chan bool),
+	}
+
+	go c.ProcessQuery()
+	go c.ReceivePacket()
+	go c.Timer()
+
+	return c, nil
 }
 
 func (c *client) ConnID() int {
-	return -1
+	return c.id // no race condition right?
 }
 
 func (c *client) Read() ([]byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return nil, errors.New("not yet implemented")
+	for {
+		c.queryChan <- Query{Type: Read}
+		msg := <-c.readChan
+		if msg.ConnID != -1 {
+			if msg.SeqNum == -1 {
+				return nil, errors.New("server disconnected")
+			}
+			return msg.Payload, nil
+		}
+	}
 }
 
 func (c *client) Write(payload []byte) error {
-	return errors.New("not yet implemented")
+	msg := &Message{
+		Type:    MsgData,
+		ConnID:  c.id,
+		Size:    len(payload),
+		Payload: payload,
+		// SeqNum, Checksum will be written in HandleWrite()
+	}
+	c.queryChan <- Query{Type: Write, msg: msg} // could block..???
+	return nil
 }
 
 func (c *client) Close() error {
-	return errors.New("not yet implemented")
+	for {
+		c.queryChan <- Query{Type: Close}
+		status := <-c.closeChan
+		if status > 0 {
+			continue
+		}
+
+		c.processingEnd <- true // shutdown ProcessQuery()
+		c.timerEnd <- true      // shutdown Timer()
+		c.conn.Close()          // shutdown ReceivePacket()
+
+		if status < 0 {
+			return errors.New("server lost connection while trying to close")
+		}
+		return nil
+	}
+}
+
+func (c *client) ProcessQuery() {
+	for {
+		select {
+		case <-c.processingEnd:
+			return
+		case q := <-c.queryChan:
+			switch q.Type {
+			case Read:
+				c.HandleRead()
+			case Write:
+				c.HandleWrite(&q)
+			case Timer:
+				c.HandleTimer()
+			case Receive:
+				c.HandleReceive(&q)
+			case Close:
+				c.HandleClose()
+			}
+		}
+	}
+}
+
+func (c *client) ReceivePacket() {
+	buffer := make([]byte, 1500)
+	for {
+		n, err := c.conn.Read(buffer)
+		if err != nil {
+			return
+		}
+
+		msg := &Message{}
+		err = json.Unmarshal(buffer[:n], msg)
+		if err != nil {
+			return
+		}
+
+		if len(msg.Payload) < msg.Size {
+			continue
+		} else if len(msg.Payload) > msg.Size {
+			// truncate
+			msg.Payload = msg.Payload[:msg.Size]
+		}
+
+		chksum := CalculateChecksum(msg.ConnID, msg.SeqNum, msg.Size, msg.Payload)
+		if chksum != msg.Checksum {
+			continue
+		}
+
+		c.queryChan <- Query{Type: Receive, msg: msg}
+	}
+}
+
+func (c *client) Timer() {
+	for {
+		timer := time.NewTimer(time.Millisecond * time.Duration(c.params.EpochMillis))
+		select {
+		case <-c.timerEnd:
+			return
+		case <-timer.C:
+			c.queryChan <- Query{Type: Timer}
+		}
+	}
+}
+
+func (c *client) HandleRead() {
+	// Check if we can read some message in order
+	msg, exist := c.server.recvBuf[c.server.readBase]
+	if exist {
+		c.readChan <- msg
+		delete(c.server.recvBuf, c.server.readBase)
+		c.server.readBase++
+		return
+	}
+
+	// Tell Read() that server is closed
+	if (c.server.status == 1 && len(c.server.sendBuf) == 0) || c.server.status == 2 {
+		c.server.status = 3
+		c.readChan <- &Message{Type: MsgData, SeqNum: -1}
+		return
+	}
+
+	// failed to read... make Read() request again
+	c.readChan <- &Message{Type: MsgData, ConnID: -1}
+}
+
+func (c *client) HandleWrite(q *Query) {
+	c.server.lastSendEpoch = c.currEpoch
+	c.server.lastSeqnum++
+
+	q.msg.SeqNum = c.server.lastSeqnum
+	q.msg.Checksum = CalculateChecksum(q.msg.ConnID, q.msg.SeqNum, q.msg.Size, q.msg.Payload)
+
+	c.server.sendBuf[q.msg.SeqNum] = &SendMessage{
+		retransmitEpoch: -1,
+		currentBackoff:  0,
+		msg:             q.msg,
+	}
+
+	c.sendPending()
+}
+
+func (c *client) HandleTimer() {
+	c.currEpoch++
+
+	if c.server.status >= 2 {
+		return
+	}
+
+	// close connection if it exceeded epoch limit
+	if c.currEpoch-c.server.lastRecvEpoch >= c.params.EpochLimit {
+		c.server.status = 2
+		return
+	}
+
+	// send heartbeat
+	if c.server.status == 0 && c.server.lastSendEpoch < c.currEpoch-1 {
+		c.sendAck(0)
+	}
+
+	// retransmit sent but unacked messages
+	for _, send_msg := range c.server.sendBuf {
+		if send_msg.retransmitEpoch == -1 {
+			continue // this message has not been sent yet!
+		} else if send_msg.retransmitEpoch <= c.currEpoch {
+			to_send, err := json.Marshal(send_msg.msg)
+			if err != nil {
+				continue
+			}
+			_, err = c.conn.Write(to_send)
+			if err != nil {
+				continue
+			}
+			send_msg.currentBackoff = nextBackoff(send_msg.currentBackoff, c.params.MaxBackOffInterval)
+			send_msg.retransmitEpoch = c.currEpoch + send_msg.currentBackoff + 1
+		}
+	}
+}
+
+func (c *client) HandleReceive(q *Query) {
+	switch q.msg.Type {
+	case MsgData:
+		c.sendAck(q.msg.SeqNum)
+
+		// Already received...!?
+		_, msg_exist := c.server.recvBuf[q.msg.SeqNum]
+		if q.msg.SeqNum < c.server.recvBase || msg_exist {
+			break
+		}
+
+		// increment recvBase if possible
+		c.server.recvBuf[q.msg.SeqNum] = q.msg
+		for {
+			_, msg_exist := c.server.recvBuf[c.server.recvBase]
+			if !msg_exist {
+				break
+			}
+			c.server.recvBase++
+		}
+
+	case MsgAck:
+		// Already received ACK or heartbeat?
+		if q.msg.SeqNum < c.server.sendBase {
+			break
+		}
+
+		delete(c.server.sendBuf, q.msg.SeqNum)
+
+		for c.server.sendBase < c.server.nextSeqnum {
+			_, msg_exist := c.server.sendBuf[c.server.sendBase]
+			if msg_exist {
+				break
+			}
+			c.server.sendBase++
+		}
+
+		c.sendPending()
+
+	case MsgCAck:
+		// Already received ACK or heartbeat?
+		if q.msg.SeqNum < c.server.sendBase {
+			break
+		}
+
+		for seqNum := c.server.sendBase; seqNum <= q.msg.SeqNum; seqNum++ {
+			delete(c.server.sendBuf, seqNum)
+		}
+
+		for c.server.sendBase < c.server.nextSeqnum {
+			_, msg_exist := c.server.sendBuf[c.server.sendBase]
+			if msg_exist {
+				break
+			}
+			c.server.sendBase++
+		}
+
+		c.sendPending()
+	}
+
+	c.server.lastRecvEpoch = c.currEpoch
+}
+
+func (c *client) HandleClose() {
+	if c.closeEpoch == -1 {
+		c.closeEpoch = c.currEpoch
+	}
+
+	unfinished := 0
+	if c.server.status == 0 {
+		c.server.status = 1
+		unfinished = 1
+	} else if c.server.status == 1 && len(c.server.sendBuf) != 0 {
+		unfinished = 1
+	}
+
+	if unfinished == 0 && c.closeEpoch <= c.timeoutCloseEpoch {
+		c.closeChan <- -1
+	} else {
+		c.closeChan <- unfinished
+	}
+}
+
+/* Send ACK to server */
+func (c *client) sendAck(seqNum int) error {
+	ack := NewAck(c.id, seqNum)
+	ack.Checksum = CalculateChecksum(ack.ConnID, ack.SeqNum, ack.Size, ack.Payload)
+	to_send, err := json.Marshal(ack)
+	if err != nil {
+		return err
+	}
+	_, err = c.conn.Write(to_send)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/* Send messages to server until window/unackedMessages is full */
+func (c *client) sendPending() error {
+	windowOccupied := c.server.nextSeqnum - c.server.sendBase
+	unackedMessages := len(c.server.sendBuf) - (c.server.lastSeqnum - c.server.nextSeqnum + 1)
+	canSend := min(c.params.WindowSize-windowOccupied, c.params.MaxUnackedMessages-unackedMessages)
+	canSend = min(canSend, c.server.lastSeqnum-c.server.nextSeqnum+1)
+	for i := 0; i < canSend; i++ {
+		send_msg := c.server.sendBuf[c.server.nextSeqnum]
+		send_msg.retransmitEpoch = c.currEpoch + 1
+		to_send, err := json.Marshal(send_msg.msg)
+		if err != nil {
+			return err
+		}
+		_, err = c.conn.Write(to_send)
+		if err != nil {
+			return err
+		}
+		c.server.nextSeqnum++
+	}
+	return nil
 }
--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -2,10 +2,36 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/cmu440/lspnet"
+)
 
 type server struct {
-	// TODO: Implement this!
+	params *Params
+	conn   *lspnet.UDPConn
+
+	currEpoch int // current time
+
+	closeEpoch        int // epoch when Close() was called
+	timeoutCloseEpoch int // epoch of the most recent timeout close of a client
+
+	numClient    int
+	client       map[string]*ConnectionInfo
+	connIdToAddr map[int]string // convert connection ID to address (string)
+
+	queryChan chan Query
+
+	readChan      chan *Message
+	closeConnChan chan bool
+	closeChan     chan int
+
+	processingEnd chan bool
+	timerEnd      chan bool
 }
 
 // NewServer creates, initiates, and returns a new server. This function should
@@ -15,23 +41,433 @@ type server struct {
 // project 0, etc.) and immediately return. It should return a non-nil error if
 // there was an error resolving or listening on the specified port number.
 func NewServer(port int, params *Params) (Server, error) {
-	return nil, errors.New("not yet implemented")
+	hostport := lspnet.JoinHostPort("localhost", strconv.Itoa(port))
+	laddr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := lspnet.ListenUDP("udp", laddr)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &server{
+		params: params,
+		conn:   conn,
+
+		currEpoch: 0,
+
+		closeEpoch:        -1,
+		timeoutCloseEpoch: -1,
+
+		numClient:    0,
+		client:       make(map[string]*ConnectionInfo),
+		connIdToAddr: make(map[int]string),
+
+		queryChan: make(chan Query),
+
+		readChan:      make(chan *Message),
+		closeConnChan: make(chan bool),
+		closeChan:     make(chan int),
+
+		processingEnd: make(chan bool),
+		timerEnd:      make(chan bool),
+	}
+
+	go s.ProcessQuery()
+	go s.ReceivePacket()
+	go s.Timer()
+
+	return s, nil
 }
 
 func (s *server) Read() (int, []byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return -1, nil, errors.New("not yet implemented")
+	for {
+		s.queryChan <- Query{Type: Read} // request read query
+		msg := <-s.readChan
+		if msg.ConnID != -1 {
+			if msg.SeqNum == -1 {
+				// client msg.ConnID disconnected
+				return msg.ConnID, nil, errors.New("disconnected")
+			}
+			return msg.ConnID, msg.Payload, nil
+		}
+	}
 }
 
 func (s *server) Write(connId int, payload []byte) error {
-	return errors.New("not yet implemented")
+	msg := &Message{
+		Type:    MsgData,
+		ConnID:  connId,
+		Size:    len(payload),
+		Payload: payload,
+		// SeqNum, Checksum will be written in HandleWrite()
+	}
+	s.queryChan <- Query{Type: Write, msg: msg} // could block..???
+	return nil
 }
 
 func (s *server) CloseConn(connId int) error {
-	return errors.New("not yet implemented")
+	msg := &Message{ConnID: connId}
+	s.queryChan <- Query{Type: CloseConn, msg: msg}
+	success := <-s.closeConnChan
+	if success {
+		return nil
+	}
+	return errors.New("connection by connId doesn't exist")
 }
 
 func (s *server) Close() error {
-	return errors.New("not yet implemented")
+	for {
+		s.queryChan <- Query{Type: Close}
+		unfinished := <-s.closeChan
+		if unfinished > 0 {
+			continue
+		}
+
+		s.processingEnd <- true // shutdown ProcessQuery()
+		s.timerEnd <- true      // shutdown Timer()
+		s.conn.Close()          // shutdown ReceivePacket()
+
+		if unfinished < 0 {
+			return errors.New("at least one connection lost while trying to close")
+		}
+		return nil
+	}
+}
+
+func (s *server) ProcessQuery() {
+	for {
+		select {
+		case <-s.processingEnd:
+			return
+		case q := <-s.queryChan:
+			switch q.Type {
+			case Read:
+				s.HandleRead()
+			case Write:
+				s.HandleWrite(&q)
+			case Timer:
+				s.HandleTimer()
+			case Receive:
+				s.HandleReceive(&q)
+			case CloseConn:
+				s.HandleCloseConn(q.msg.ConnID)
+			case Close:
+				s.HandleClose()
+			}
+		}
+
+	}
+}
+
+func (s *server) ReceivePacket() {
+	buffer := make([]byte, 1500)
+	for {
+		n, addr, err := s.conn.ReadFromUDP(buffer)
+		if err != nil {
+			return
+		}
+
+		msg := &Message{}
+		err = json.Unmarshal(buffer[:n], msg)
+		if err != nil {
+			return
+		}
+
+		if len(msg.Payload) < msg.Size {
+			continue
+		} else if len(msg.Payload) > msg.Size {
+			// truncate
+			msg.Payload = msg.Payload[:msg.Size]
+		}
+
+		chksum := CalculateChecksum(msg.ConnID, msg.SeqNum, msg.Size, msg.Payload)
+		if chksum != msg.Checksum {
+			continue
+		}
+
+		s.queryChan <- Query{Type: Receive, addr: addr, msg: msg}
+	}
+}
+
+func (s *server) Timer() {
+	for {
+		timer := time.NewTimer(time.Millisecond * time.Duration(s.params.EpochMillis))
+		select {
+		case <-s.timerEnd:
+			return
+		case <-timer.C:
+			s.queryChan <- Query{Type: Timer}
+		}
+	}
+}
+
+func (s *server) HandleRead() {
+	for _, cli := range s.client {
+		// Check if we can read some message in order
+		msg, exist := cli.recvBuf[cli.readBase]
+		if exist {
+			s.readChan <- msg
+			delete(cli.recvBuf, cli.readBase)
+			cli.readBase++
+			return
+		}
+
+		// Tell Read() that client is closed
+		if (cli.status == 1 && len(cli.sendBuf) == 0) || cli.status == 2 {
+			cli.status = 3
+			s.readChan <- &Message{Type: MsgData, ConnID: cli.id, SeqNum: -1}
+			return
+		}
+	}
+	// failed to read... make Read() request again
+	s.readChan <- &Message{Type: MsgData, ConnID: -1}
+}
+
+func (s *server) HandleWrite(q *Query) {
+	// Sanity check
+	addr, exist := s.connIdToAddr[q.msg.ConnID]
+	if !exist {
+		panic("What the fuck?")
+	}
+	cli, exist := s.client[addr]
+	if !exist {
+		panic("What the fuck?")
+	}
+
+	cli.lastSeqnum++
+	q.msg.SeqNum = cli.lastSeqnum
+	q.msg.Checksum = CalculateChecksum(q.msg.ConnID, q.msg.SeqNum, q.msg.Size, q.msg.Payload)
+
+	cli.sendBuf[q.msg.SeqNum] = &SendMessage{
+		retransmitEpoch: -1,
+		currentBackoff:  0,
+		msg:             q.msg,
+	}
+
+	s.sendPending(cli)
+}
+
+func (s *server) HandleTimer() {
+	s.currEpoch++
+
+	for _, cli := range s.client {
+		if cli.status >= 2 {
+			continue
+		}
+
+		// close connection that exceeded epoch limit
+		if s.currEpoch-cli.lastRecvEpoch >= s.params.EpochLimit {
+			s.timeoutCloseEpoch = s.currEpoch
+			cli.status = 2
+			continue
+		}
+
+		// send heartbeat to client
+		if cli.status == 0 && cli.lastSendEpoch < s.currEpoch-1 {
+			s.sendAck(cli, 0)
+		}
+
+		// retransmit sent but unacked messages
+		for _, send_msg := range cli.sendBuf {
+			if send_msg.retransmitEpoch == -1 {
+				continue // message not sent yet!
+			} else if send_msg.retransmitEpoch <= s.currEpoch {
+				to_send, err := json.Marshal(send_msg.msg)
+				if err != nil {
+					continue
+				}
+				_, err = s.conn.WriteToUDP(to_send, &cli.addr)
+				if err != nil {
+					continue
+				}
+				send_msg.currentBackoff = nextBackoff(send_msg.currentBackoff, s.params.MaxBackOffInterval)
+				send_msg.retransmitEpoch = s.currEpoch + send_msg.currentBackoff + 1
+			}
+		}
+	}
+}
+
+func (s *server) HandleReceive(q *Query) {
+	cli, cli_exist := s.client[q.addr.String()]
+
+	switch q.msg.Type {
+	case MsgConnect:
+		/* Setup new connection */
+		if !cli_exist {
+			s.numClient++
+			cli = &ConnectionInfo{
+				status: 0,
+
+				id:   s.numClient,
+				addr: *q.addr,
+
+				lastRecvEpoch: s.currEpoch,
+				lastSendEpoch: s.currEpoch,
+
+				sendBase:   1,
+				nextSeqnum: 1,
+				lastSeqnum: 0,
+				sendBuf:    make(map[int]*SendMessage),
+
+				recvBase: q.msg.SeqNum + 1,
+				readBase: q.msg.SeqNum + 1,
+				recvBuf:  make(map[int]*Message),
+			}
+			s.client[q.addr.String()] = cli
+			s.connIdToAddr[cli.id] = q.addr.String()
+		}
+		s.sendAck(cli, q.msg.SeqNum) // send connection ack
+
+	case MsgData:
+		if !cli_exist {
+			panic("What the fuck")
+		}
+
+		s.sendAck(cli, q.msg.SeqNum)
+
+		// Already received...!?
+		_, msg_exist := cli.recvBuf[q.msg.SeqNum]
+		if q.msg.SeqNum < cli.recvBase || msg_exist {
+			break
+		}
+
+		// increment recvBase if possible
+		cli.recvBuf[q.msg.SeqNum] = q.msg
+		for {
+			_, msg_exist := cli.recvBuf[cli.recvBase]
+			if !msg_exist {
+				break
+			}
+			cli.recvBase++
+		}
+
+	case MsgAck:
+		if !cli_exist {
+			panic("What the fuck?")
+		}
+
+		// Already received ACK or heartbeat?
+		if q.msg.SeqNum < cli.sendBase {
+			break
+		}
+
+		// Delete seqnum from sendBuf
+		delete(cli.sendBuf, q.msg.SeqNum)
+
+		// Increment sendBase until pending seqnum
+		for cli.sendBase < cli.nextSeqnum {
+			_, msg_exist := cli.sendBuf[cli.sendBase]
+			if msg_exist {
+				break
+			}
+			cli.sendBase++
+		}
+
+		s.sendPending(cli)
+
+	case MsgCAck:
+		if !cli_exist {
+			panic("What the fuck?")
+		}
+
+		// Already received ACK or heartbeat?
+		if q.msg.SeqNum < cli.sendBase {
+			break
+		}
+
+		// Delete seqnum from sendBuf
+		for seqNum := cli.sendBase; seqNum <= q.msg.SeqNum; seqNum++ {
+			delete(cli.sendBuf, seqNum)
+		}
+
+		// Increment sendBase until pending seqnum
+		for cli.sendBase < cli.nextSeqnum {
+			_, msg_exist := cli.sendBuf[cli.sendBase]
+			if msg_exist {
+				break
+			}
+			cli.sendBase++
+		}
+
+		s.sendPending(cli)
+	}
+
+	cli.lastRecvEpoch = s.currEpoch
+}
+
+func (s *server) HandleCloseConn(connId int) {
+	addr, exist := s.connIdToAddr[connId]
+	if !exist {
+		s.closeConnChan <- false
+		return
+	}
+	cli, exist := s.client[addr]
+	if !exist {
+		s.closeConnChan <- false
+		return
+	}
+	cli.status = 1
+	s.closeConnChan <- true
+}
+
+func (s *server) HandleClose() {
+	if s.closeEpoch == -1 {
+		s.closeEpoch = s.currEpoch
+	}
+
+	unfinished := 0
+	for _, cli := range s.client {
+		if cli.status == 0 {
+			cli.status = 1
+			unfinished += 1
+		} else if cli.status == 1 && len(cli.sendBuf) != 0 {
+			unfinished += 1
+		}
+	}
+
+	if unfinished == 0 && s.closeEpoch <= s.timeoutCloseEpoch {
+		s.closeChan <- -1
+	} else {
+		s.closeChan <- unfinished
+	}
+}
+
+/* Send ACK to client */
+func (s *server) sendAck(cli *ConnectionInfo, seqNum int) error {
+	ack := NewAck(cli.id, seqNum)
+	ack.Checksum = CalculateChecksum(ack.ConnID, ack.SeqNum, ack.Size, ack.Payload)
+	to_send, err := json.Marshal(ack)
+	if err != nil {
+		return err
+	}
+	_, err = s.conn.WriteToUDP(to_send, &cli.addr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/* Send messages to client until window/unackedMessages is full */
+func (s *server) sendPending(cli *ConnectionInfo) error {
+	windowOccupied := cli.nextSeqnum - cli.sendBase
+	unackedMessages := len(cli.sendBuf) - (cli.lastSeqnum - cli.nextSeqnum + 1)
+	canSend := min(s.params.WindowSize-windowOccupied, s.params.MaxUnackedMessages-unackedMessages)
+	canSend = min(canSend, cli.lastSeqnum-cli.nextSeqnum+1)
+	for i := 0; i < canSend; i++ {
+		cli.lastSendEpoch = s.currEpoch
+		send_msg := cli.sendBuf[cli.nextSeqnum]
+		send_msg.retransmitEpoch = s.currEpoch + 1
+		to_send, err := json.Marshal(send_msg.msg)
+		if err != nil {
+			return err
+		}
+		_, err = s.conn.WriteToUDP(to_send, &cli.addr)
+		if err != nil {
+			return err
+		}
+		cli.nextSeqnum++
+	}
+	return nil
 }
